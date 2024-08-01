1063c9a5f8361e61ef787bb1e6526dec6a42f317
FlorHeredia <florheredia95@hotmail.es>
2024-07-31 23:12:52 -0300

    ultima version

--- contacto.js
+++ contacto.js
@@ -1,22 +1,3 @@
-// let nombre = document.getElementById("nombre");
-// let apellido = document.getElementById("apellido");
-// let btnEnviar = document.getElementById("enviar");
-// let informacion = [];
-
-
-// btnEnviar.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     informacion[0] = nombre.value;
-//     informacion[1] = apellido.value;
-
-//     console.log(`Su nombre es ${informacion[0]} y su apellido es ${informacion[1]}`);
-//     let blob = new Blob([informacion], { type: "text/plain;charset=utf-8" });
-
-//     saveAs(blob, "contacto.txt");
-// })
-
-//-------------------------------------------------------------------------
-
 let nombre = document.getElementById("nombre");
 let apellido = document.getElementById("apellido");
 let telefono = document.getElementById ("telefono");
--- productos.js
+++ productos.js
@@ -1,35 +1,55 @@
 let arrProductos= ["Fideos Matarazzo", "Azúcar Chango", "Yerba Mate", "Vino", "Pure De Tomate",
     "Oreo", "Leche", "Aceite", "Arroz", "Café", "Lavandina", "Cacao"];
-let arrPrecio=   [1400, 1390, 4865, 4950, 700, 3850, 1230, 1850, 1150, 6650, 1150, 1390];
-let arrCant=      [100, 50, 75, 60, 90, 100, 80, 70, 50, 40, 100, 120];
+let arrPrecios=   [1400, 1390, 4865, 4950, 700, 3850, 1230, 1850, 1150, 6650, 1150, 1390];
+let arrStock=  [100, 50, 75, 60, 90, 100, 80, 70, 50, 40, 100, 120];
+let arrImage =["./img/FIDEOS.webp", "./img/AZUCAR.webp" ,"img/yerba.webp", "img/VINO.webp","img/PURE DE TOMATES.webp", "img/OREO.webp", "img/LECHE.webp","img/ACEITE.webp", "img/ARROZ-GALLO.webp", "img/CAFE.webp", "img/LAVANDINA.webp", "img/CACAO.webp" ]
+let totalCompra = 0;
 
-document.querySelector("div button").addEventListener("click", ()=>{
-    let cantidad=document.querySelector("div input").value;
-    cantidad=Number(cantidad);
-    if(cantidad>0&&(arrCant[0]-cantidad)>0){
-        comprar(cantidad);
+function cargarProductos(arrProductos, arrPrecios, arrStock,arrImage){
+      let div=document.getElementById("contenedor-general");
+     for(let i=0; i<arrProductos.length;i++){
+        div.innerHTML +=
+        `
+        <div class="cajas">
+            <figure><img class="imagenes" src="${arrImage[i]}" alt="${arrProductos[i]}"></img></figure>
+            <div class="info-producto">
+                <h2>${arrProductos[i]}</h2>
+                <p class="precio"> $${arrPrecios[i]}</p>
+                <p> Stock disponible:</p><p type="number" id="stock${i}"> ${arrStock[i]}</p>
+            </div>
+            <div class="button">
+        
+                <input type="number" id="cant${i}" placeholder="0">
+                <button class="button" id="btn${i}">Comprar</button>
+            </div>
+        </div>
+        
+        
+    `;
+     }
+}
+cargarProductos(arrProductos,arrPrecios,arrStock,arrImage);
+function comprar(index){
+    let stockHTML = document.getElementById(`stock${index}`);
+    let cantidadHTML = document.getElementById(`cant${index}`);
+    let stock= parseInt(stockHTML.innerText);
+    let cantProd=parseInt(cantidadHTML.value);
+    if(cantProd>0 && cantProd <= stock){
+        stockHTML.innerText= stock-cantProd;
+        cantidadHTML.value ="";
+        alert("Compra realizada exitosamente");
+         
     }else{
-        alert(" Por favor, ingrese una cantidad mayor a cero o no hay stock")
+        alert("Cantidad no valida, debe ser mayor a 0 o menor/igual que el stock")
     }
-})
-
-function comprar(cantidad) {
-    document.getElementById("total").innerText=cantidad*arrPrecio[0];
+}
+for (let i = 0; i < arrProductos.length; i++) {
+    document.getElementById(`btn${i}`).addEventListener("click", () => {
+        comprar(i);
+    })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function mostrarTotalCompra(){
+    let elementoTotal = document.getElementById("total"); // Reemplaza con el ID del elemento donde mostrarás el total
+    elementoTotal.textContent = `Total de la compra: $${totalCompra}`;
+}
